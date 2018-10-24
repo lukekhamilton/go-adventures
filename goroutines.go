ab121b93a87e40dadbc213c88a0fea0c4b0c5cc9
lukekhamilton <luke@mettadata.com>
2018-10-25 09:21:32 +1100

    Goroutines

---
+++ goroutines.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func f(from string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(from, ":", i)
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+func main() {
+
+	for x := 0; x < 5; x++ {
+		go f("goroutine1")
+
+		go f("goroutine2")
+	}
+	go f("goroutine3")
+
+	go f("goroutine4")
+
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	fmt.Scanln()
+	fmt.Println("done")
+}
