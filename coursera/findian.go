7ae36fc72e9f518ee889b42d5135e0d1e4226206
lukekhamilton <luke@mettadata.com>
2018-10-31 00:10:12 +1100

    Maken a spot for my coursera work too

--- coursera/findian.go
+++ coursera/findian.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 )
 
+var xxx = 123
+
 // Findian ...
 func Findian() {
 	var str string
@@ -13,12 +15,14 @@ func Findian() {
 
 func XXX() {
 	var xtemp int
+	fmt.Println(xxx)
 	x1 := 0
 	x2 := 1
 	for x := 0; x < 5; x++ {
 		xtemp = x2
 		x2 = x2 + x1
 		x1 = xtemp
+		xxx += x1 + x2 + xtemp
 	}
 	fmt.Println(x2)
 }
