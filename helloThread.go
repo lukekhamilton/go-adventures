babd7923aa7fc0e60935487bc28415f96e8ed9b4
Luke Hamilton <luke@mettadata.com>
2018-11-02 21:42:40 +1100

    Just playing more with threads

--- helloThread.go
+++ helloThread.go
@@ -1,14 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+var channel = make(chan string)
+var counter int
+
+func hello(msg string) {
+	counter++
+
+	// time.Sleep(time.Millisecond * 500)
+	channel <- msg + " " + strconv.Itoa(counter)
+	return
+}
 
 func main() {
-	channel := make(chan string)
 
-	go func(msg string) {
-		channel <- msg
-	}("Hello Threads")
+	var x = hello
 
-	fmt.Println(<-channel)
+	for {
+		// fmt.Println("Starting...")
+		go x("Hello World ThreadX")
+		go x("Hello World ThreadY")
+		go x("Hello World ThreadZ")
+		fmt.Println(<-channel)
+	}
+
+	// go func(msg string) {
+	// 	channel <- msg
+	// }("Hello Threads")
 
 }
