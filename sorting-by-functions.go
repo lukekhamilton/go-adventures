49de6b9aee51eb1e7480c5f8c983c0f1fff0898a
Luke Hamilton <luke@mettadata.com>
2018-11-06 17:57:07 +1100

    Added custom sorting via interface

---
+++ sorting-by-functions.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type byLength []string
+
+func (s byLength) Len() int {
+	return len(s)
+}
+
+func (s byLength) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s byLength) Less(i, j int) bool {
+	return len(s[i]) < len(s[j])
+}
+
+func main() {
+
+	fruits := []string{"peace", "banana", "kiwi"}
+	fmt.Println(fruits)
+
+	sort.Strings(fruits)
+	fmt.Println(fruits)
+
+	sort.Sort(byLength(fruits))
+	fmt.Println(fruits)
+}
