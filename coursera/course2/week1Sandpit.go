e7301cbab545f2cd2db1be7126f6991bfae350ce
Luke Hamilton <luke@mettadata.com>
2018-11-06 17:43:58 +1100

    Added week1 course2

---
+++ coursera/course2/week1Sandpit.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// func foo(y *int) {
+// 	*y = *y + 1
+// }
+
+// func passPointer() {
+
+// 	x := 2
+// 	foo(&x)
+// 	fmt.Println(x)
+// }
+
+// func passArray(x [3]]int) int {
+// 	return x[3]
+// }
+
+func sliceFoo(sli []int) {
+	sli[0] = sli[0] + 1
+}
+
+func main() {
+	a := []int{1, 2, 3}
+	sliceFoo(a)
+	fmt.Println(a)
+}
