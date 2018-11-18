0400417bb99aa05ad9666d2eb0e956bbff10c964
Luke Hamilton <luke@mettadata.com>
2018-11-18 18:15:59 +1100

    Renamed functions to stop namespace clash

--- goByExample/methods.go
+++ goByExample/methods.go
@@ -4,21 +4,21 @@ import (
 	"fmt"
 )
 
-type rect struct {
+type rectangle struct {
 	width, height int
 }
 
-func (r *rect) area() int {
+func (r *rectangle) area() int {
 	return r.width * r.height
 }
 
-func (r rect) perim() int {
+func (r rectangle) perim() int {
 	return 2*r.width + 2*r.height
 }
 
 // Methods ...
 func Methods() {
-	r := rect{width: 10, height: 5}
+	r := rectangle{width: 10, height: 5}
 
 	fmt.Println("area: ", r.area())
 	fmt.Println("perim: ", r.perim())
