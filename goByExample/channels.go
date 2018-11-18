995b0b730c68a3b4c683283d112853373b43700a
Luke Hamilton <luke@mettadata.com>
2018-11-18 18:55:14 +1100

    Channel blocker

--- goByExample/channels.go
+++ goByExample/channels.go
@@ -2,11 +2,13 @@ package gobyexample
 
 import "fmt"
 
+// Channels ...
 func Channels() {
 	messages := make(chan string)
 
 	go func() { messages <- "ping" }()
 
-	msg := <-messages
-	fmt.Println(msg)
+	// msg := <-messages
+	fmt.Println(<-messages) // Prints chan
+	fmt.Println(<-messages) // blocks chan
 }
