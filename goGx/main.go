ea403ef7271d0ec0130a7878af40aa35f965689b
Luke Hamilton <luke@mettadata.com>
2018-11-24 00:44:51 +1100

    gx rm

--- goGx/main.go
+++ goGx/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	log "gx/ipfs/QmcuXC5cxs79ro2cUuHs4HQ2bkDLJUYokwL8aivcX6HW3C/go-log"
+)
 
 func main() {
+	log.Println("Hello World!")
 	fmt.Println("Hello world!")
+
 }
