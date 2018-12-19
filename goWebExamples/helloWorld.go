f275b41d1276af6a61028c2167a70a12096c8117
Luke Hamilton <luke@mettadata.com>
2018-12-20 10:25:29 +1100

    http examples

--- goWebExamples/helloWorld.go
+++ goWebExamples/helloWorld.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func main() {
+func hello() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 	})
