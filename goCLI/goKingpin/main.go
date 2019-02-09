23921f7210f163ba51c6edfa1458dde24f1d40bc
Luke Hamilton <luke@mettadata.com>
2019-02-09 17:13:35 +1100

    Added full example of kingpin package

--- goCLI/goKingpin/main.go
+++ goCLI/goKingpin/main.go
@@ -18,6 +18,8 @@ func addSubCommand(app *kingpin.Application, name string, description string) {
 func main() {
 	app := kingpin.New("goKingpin", "My pretend app CLI with Bash completion")
 
+	configureNetCatCommand(app)
+
 	app.Flag("flag-1", "").String()
 	app.Flag("flag-2", "").HintOptions("opts1", "opts2").String()
 
@@ -27,4 +29,42 @@ func main() {
 	addSubCommand(app, "nmap", "Additional top level command to show command completion")
 
 	kingpin.MustParse(app.Parse(os.Args[1:]))
+}
+
+// NetcatCommand ...
+type NetcatCommand struct {
+	hostName string
+	port     int
+	format   string
+}
+
+func (n *NetcatCommand) run(c *kingpin.ParseContext) error {
+	fmt.Printf("Would have run netcat to hostname %v, port %d, and output format %v\n", n.hostName, n.port, n.format)
+	return nil
+}
+
+func configureNetCatCommand(app *kingpin.Application) {
+	c := &NetcatCommand{}
+	nc := app.Command("nc", "Connect to a host").Action(c.run)
+	nc.Flag("nop-flag", "Example of a flag with no options").Bool()
+
+	// You can provide hint options statically
+	nc.Flag("port", "Provide a port to connect to").
+		Required().
+		HintOptions("80", "443", "8080").
+		IntVar(&c.port)
+
+	// Enum/EnumVar options will be turned into completion options automatically
+	nc.Flag("format", "Define the output format").
+		Default("raw").
+		EnumVar(&c.format, "raw", "json")
+
+	nc.Flag("host", "Provide a hostnanme to nc").
+		Required().
+		HintAction(listHosts).
+		StringVar(&c.hostName)
+}
+
+func listHosts() []string {
+	return []string{"sshost.exmapkle", "webhost.Example", "ftphost.Example"}
 }
