f4b779767dcd464bf331d1c713c4848ad0e62ada
Luke Hamilton <luke@mettadata.com>
2019-08-14 13:10:39 +1000

    * Updated prometheus poc

--- prometheus/client_golang/main.go
+++ prometheus/client_golang/main.go
@@ -1,31 +1,67 @@
+// Ref: https://sysdig.com/blog/prometheus-metrics/
 package main
 
 import (
+	"fmt"
+	"math/rand"
 	"net/http"
 	"time"
 
-	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/log"
+
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/prometheus/client_golang/prometheus"
 )
 
-var fooCount = prometheus.NewCounter(prometheus.CounterOpts{
-	Name: "foo_total",
-	Help: "Number of foo successfully processed.",
-})
+var (
+	counter = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: "golang",
+		Name:      "my_counter",
+		Help:      "This is my counter",
+	})
 
-func foo() {
-	for {
-		time.Sleep(time.Second)
-		fooCount.Add(1)
-	}
-}
+	gauge = prometheus.NewGauge(prometheus.GaugeOpts{
+		Namespace: "golang",
+		Name:      "my_gauge",
+		Help:      "This is my gauge",
+	})
+
+	histogram = prometheus.NewHistogram(prometheus.HistogramOpts{
+		Namespace: "golang",
+		Name:      "my_histogram",
+		Help:      "This is my histogram",
+	})
+
+	summary = prometheus.NewSummary(prometheus.SummaryOpts{
+		Namespace: "golang",
+		Name:      "my_summary",
+		Help:      "This is my summary",
+	})
+)
 
 func main() {
-	prometheus.MustRegister(fooCount)
+	fmt.Println("booting...")
+	rand.Seed(time.Now().Unix())
 
-	go foo()
+	prometheus.MustRegister(counter)
+	prometheus.MustRegister(gauge)
+	prometheus.MustRegister(histogram)
+	prometheus.MustRegister(summary)
 
 	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":9090", nil)
 
+	go func() {
+		for {
+			fmt.Println("Generating stats...")
+			counter.Add(1)
+			gauge.Add(1)
+			histogram.Observe(2)
+			summary.Observe(2)
+
+			time.Sleep(time.Second)
+		}
+	}()
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
